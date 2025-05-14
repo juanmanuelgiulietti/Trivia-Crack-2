a99099b09a0312670f2742339892bc85303609b8
Juan Manuel Giulietti <juanmanuelgiulietti2@gmail.com>
2025-05-13 21:23:38 -0300

    Comenzar a jugar

--- main.py
+++ main.py
@@ -1,3 +1,68 @@
+import random
+import json
+import time
+
+def comprobarResultado(pregunta, turno, nombre):
+    respuesta = int(input("Ingrese su respuesta: "))
+    while respuesta not in [1,2,3,4]:
+        print("❌ Opción inválida. Por favor, seleccioná un número dentro de las opciones.\n")
+    opcionSeleccionada = pregunta["opciones"][respuesta - 1]
+    
+    if opcionSeleccionada == pregunta["respuesta_correcta"]:
+        print("¡Correcto! ✔️")
+        print(f"🧠 La respuesta era: {pregunta["respuesta_correcta"]}")
+        return turno
+    else:
+        print("❌ ¡Incorrecto! ")
+        print(f"🧠 La respuesta era: {pregunta["respuesta_correcta"]}")
+        if turno == nombre:
+            return "Computadora"
+        else:
+            return nombre
+        
+def generarPreguntasYMostrarlas(categoria):
+    with open("preguntas.json", "r", encoding="utf-8") as archivo:
+        preguntas = json.load(archivo)
+    preguntasPorCategoria = [pregunta for pregunta in preguntas if pregunta["categoria"].lower() == categoria.lower()]
+    for pregunta in preguntasPorCategoria:
+        print(f"📚 Categoría: {categoria}")
+        print(f"🤔 Pregunta: {pregunta["pregunta"]}")
+        for i, opcion in enumerate(pregunta["opciones"], 1):
+            print(f"{i}. {opcion}")
+    return pregunta
+
+def girarRuleta():
+    categorias = ["Arte", "Geografia", "Historia", "Deportes", "Entretenimiento", "Ciencia"]
+    emojis_categorias = {
+    "Arte": "🎨",
+    "Ciencia": "🧪",
+    "Deportes": "⚽",
+    "Entretenimiento": "🎬",
+    "Geografia": "🌍",
+    "Historia": "🏺",
+    "Corona": "👑"
+    }
+    categoria = random.choice(categorias)
+    print("🎡 Girando la ruleta...")
+    time.sleep(1)
+    print(f"🎡 La ruleta giró... ¡y cayó en {emojis_categorias[categoria]} {categoria.capitalize()}!")
+    return categoria
+
+def anunciarTurno(nombre, turno):
+    if turno == "Computadora":
+        print("🤖 Turno de la computadora...\n")
+    else:
+        print(f"👤 Es tu turno, {nombre}!\n")
+        
+# Comenzar a jugar (Modo Clasico)
+def modoClasico(nombre, turno):    
+    anunciarTurno(nombre, turno)
+    categoria = girarRuleta()
+    pregunta = generarPreguntasYMostrarlas(categoria)
+    turno = comprobarResultado(pregunta, turno, nombre)
+    time.sleep(2)
+    
+# Dar bienvenida al usuario
 def darBienvenida():
     nombre = str(input("Ingrese su nombre: ")).capitalize()
     while nombre == "":
@@ -15,4 +80,14 @@ def main():
     print("¡Escoge tu modo de juego!\n")
     for i, modo in enumerate(modosDeJuego, 1):
         print(f"{i}. {modo}")
+        
+    # Comenzar a jugar
+    jugar = int(input("Escoge el modo de juego que desee: "))
+    while jugar not in [1, 2]:
+        print("❌ Opción inválida. Por favor, seleccioná un número del menú.\n")
+    turno = nombre
+    if jugar == 1:
+        modoClasico(nombre, turno)
+    else:
+        print("🔧 Modo Desafío Diario aún no está disponible.")
 main()
